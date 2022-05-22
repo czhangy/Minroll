bc516f31dfee5ad2bb8d8e529aa90daf4e97c8b9
Charles Zhang <charleszhangmb@gmail.com>
2022-05-22 15:07:53 -0700

    Prettier setup

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const path = require("path");
 
 const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-},
-}
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, "styles")],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
