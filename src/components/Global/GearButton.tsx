7224067a666a4be9467d658f9f55f8923d2991be
Charles Zhang <charleszhangmb@gmail.com>
2022-05-30 14:53:02 -0700

    db setup

--- src/components/Global/GearButton.tsx
+++ src/components/Global/GearButton.tsx
@@ -18,18 +18,7 @@ const GearButton: React.FC<Props> = ({ type }: Props) => {
             button.classList.remove(styles["active"]);
         // Activate clicked button
         (e.target as HTMLButtonElement).classList.add(styles["active"]);
-
-        const newGear = {
-            name: "Warhelm of Kassar",
-            category: "head",
-            rarity: "legendary",
-            effect: "Reduces the cooldown and increase the damage of Phalanx by 60%",
-            description:
-                "At Akkhan's request, Kassar trained the first Crusaders, turning them into skillful warriors through his rigorous methods. His own apprentices faced harsher trials and were each lost in training, unable to carry on his name. — Abd al-Hazir, The Crusaders",
-            src: "https://static.wikia.nocookie.net/diablo/images/b/b1/KassarHelm.png/revision/latest?cb=20160115152518",
-        };
-
-        await axios.post("/api/gear", newGear);
+        await axios.get("/api/gear");
     };
 
     return (
