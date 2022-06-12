d9c5afb39cad900a303c5a433ae91fb86cc035b0
Charles Zhang <charleszhangmb@gmail.com>
2022-06-12 15:03:28 -0700

    done

--- src/components/Planner/Dropdown.tsx
+++ src/components/Planner/Dropdown.tsx
@@ -6,6 +6,8 @@ import { useState, useEffect } from "react";
 import { SyntheticEvent } from "react";
 // Next
 import Image from "next/image";
+// Local component
+import ResetModal from "@/components/Planner/ResetModal";
 
 type Props = {
     content: string[];
@@ -42,7 +44,9 @@ const Dropdown: React.FC<Props> = (props: Props) => {
     }, [props.savedValue]);
 
     // Reset build
+    const [resetModalOpen, setResetModalOpen] = useState<boolean>(false);
     const resetBuild = () => {
+        setResetModalOpen(false);
         setSelectedValue(null);
         (props.onReset as () => void)();
     };
@@ -56,102 +60,113 @@ const Dropdown: React.FC<Props> = (props: Props) => {
     };
 
     return (
-        <div className={styles.dropdown}>
-            {props.onReset && selectedValue ? (
-                <div className={styles["dropdown-button"]}>
-                    <div className={styles["dropdown-selection"]}>
-                        <Image
-                            src={`/icons/${selectedValue}.webp`}
-                            alt=""
-                            height={25}
-                            width={30}
-                        />
-                        <p className={styles["dropdown-text"]}>
-                            {formatValue(selectedValue)}
-                        </p>
-                    </div>
-                    <button
-                        className={styles["reset-button"]}
-                        onClick={resetBuild}
-                    >
-                        <Image
-                            src={
-                                props.isLoading
-                                    ? "/icons/loading.gif"
-                                    : "/icons/reset.svg"
-                            }
-                            alt=""
-                            layout="fill"
-                            objectFit="contain"
-                        />
-                    </button>
-                </div>
+        <>
+            {props.onReset ? (
+                <ResetModal
+                    open={resetModalOpen}
+                    onConfirm={resetBuild}
+                    onClose={() => setResetModalOpen(false)}
+                />
             ) : (
-                <button
-                    className={styles["dropdown-button"]}
-                    onClick={openDropdown}
-                    onBlur={closeDropdown}
-                    disabled={props.isLoading}
-                >
-                    {selectedValue ? (
+                ""
+            )}
+            <div className={styles.dropdown}>
+                {props.onReset && selectedValue ? (
+                    <div className={styles["dropdown-button"]}>
                         <div className={styles["dropdown-selection"]}>
+                            <Image
+                                src={`/icons/${selectedValue}.webp`}
+                                alt=""
+                                height={25}
+                                width={30}
+                            />
                             <p className={styles["dropdown-text"]}>
                                 {formatValue(selectedValue)}
                             </p>
                         </div>
-                    ) : (
-                        <p
-                            className={`${styles["dropdown-text"]} ${styles.placeholder}`}
+                        <button
+                            className={styles["reset-button"]}
+                            onClick={() => setResetModalOpen(true)}
                         >
-                            {props.placeholder}
-                        </p>
-                    )}
-                    <div
-                        className={`${styles["dropdown-icon"]} ${
-                            open ? styles.rotated : ""
-                        }`}
-                    >
-                        <Image
-                            src="/icons/chevron-down.svg"
-                            alt=""
-                            layout="fill"
-                            objectFit="contain"
-                        />
+                            <Image
+                                src={
+                                    props.isLoading
+                                        ? "/icons/loading.gif"
+                                        : "/icons/reset.svg"
+                                }
+                                alt=""
+                                layout="fill"
+                                objectFit="contain"
+                            />
+                        </button>
                     </div>
-                </button>
-            )}
-            <ul
-                className={`${styles["dropdown-options"]} ${
-                    open ? styles.show : ""
-                }`}
-            >
-                {props.content.map((value, i) => {
-                    return (
-                        <li
-                            className={styles["dropdown-option"]}
-                            key={i}
-                            onClick={() => selectValue(value)}
-                        >
-                            {props.hasIcon ? (
-                                <div className={styles["option-image"]}>
-                                    <Image
-                                        src={`/icons/${value}.webp`}
-                                        alt=""
-                                        layout="fill"
-                                        objectFit="contain"
-                                    />
-                                </div>
-                            ) : (
-                                ""
-                            )}
-                            <p className={styles["option-text"]}>
-                                {formatValue(value)}
+                ) : (
+                    <button
+                        className={styles["dropdown-button"]}
+                        onClick={openDropdown}
+                        onBlur={closeDropdown}
+                        disabled={props.isLoading}
+                    >
+                        {selectedValue ? (
+                            <div className={styles["dropdown-selection"]}>
+                                <p className={styles["dropdown-text"]}>
+                                    {formatValue(selectedValue)}
+                                </p>
+                            </div>
+                        ) : (
+                            <p
+                                className={`${styles["dropdown-text"]} ${styles.placeholder}`}
+                            >
+                                {props.placeholder}
                             </p>
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
+                        )}
+                        <div
+                            className={`${styles["dropdown-icon"]} ${
+                                open ? styles.rotated : ""
+                            }`}
+                        >
+                            <Image
+                                src="/icons/chevron-down.svg"
+                                alt=""
+                                layout="fill"
+                                objectFit="contain"
+                            />
+                        </div>
+                    </button>
+                )}
+                <ul
+                    className={`${styles["dropdown-options"]} ${
+                        open ? styles.show : ""
+                    }`}
+                >
+                    {props.content.map((value, i) => {
+                        return (
+                            <li
+                                className={styles["dropdown-option"]}
+                                key={i}
+                                onClick={() => selectValue(value)}
+                            >
+                                {props.hasIcon ? (
+                                    <div className={styles["option-image"]}>
+                                        <Image
+                                            src={`/icons/${value}.webp`}
+                                            alt=""
+                                            layout="fill"
+                                            objectFit="contain"
+                                        />
+                                    </div>
+                                ) : (
+                                    ""
+                                )}
+                                <p className={styles["option-text"]}>
+                                    {formatValue(value)}
+                                </p>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </>
     );
 };
 
