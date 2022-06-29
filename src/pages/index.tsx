ef8c7b57fb61704c676b3aaa4581ca983f1716e1
Charles Zhang <charleszhangmb@gmail.com>
2022-06-28 22:37:29 -0700

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,19 +10,23 @@ import styles from "@/styles/Home/Home.module.scss";
 import { useAuth } from "@/contexts/AuthContext";
 
 const Home: NextPage = () => {
+    // Hook
     const { user } = useAuth() as AuthContext;
 
     return (
         <div id={styles.home}>
+            {/* Metadata */}
             <Head>
                 <title>Home | Minroll</title>
             </Head>
             <div id={styles["home-container"]}>
+                {/* Main text */}
                 <p id={styles["home-text"]}>
                     Welcome to
                     <br />
                     <span id={styles["site-title"]}>Minroll</span>
                 </p>
+                {/* Link to login/profile (based on user status) */}
                 <Link href={user ? "/profile" : "/login"}>
                     <a id={styles["get-started"]}>Get Started</a>
                 </Link>
